7634b41b40a37643881a43bcbe2f65e5af7a5996
Tyler Rafferty <tjraff5@gmail.com>
2026-04-28 13:25:34 +0200

    updated sitemap

--- frontend/src/app/privacy/layout.js
+++ frontend/src/app/privacy/layout.js
@@ -1,6 +1,9 @@
 export const metadata = {
   title: "Privacy Policy",
   description: "Privacy Policy for CustomGuess.",
+  alternates: {
+    canonical: "https://customguess.com/privacy",
+  },
 };
 
 export default function PrivacyLayout({ children }) {
